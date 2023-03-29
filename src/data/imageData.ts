4685736cb1cfa5456cde41a50a39184098ee7ba7
DiyankaGovender <diyanka@renderheads.com>
2023-03-29 15:45:01 +0200

    Blank image added

--- src/data/imageData.ts
+++ src/data/imageData.ts
@@ -24,13 +24,185 @@ export enum ImageType {
 let imageData: IImageData = {
 
     //#region Debug Images
-
-    sky: {
+    bear: {
         type: ImageType.Image,
-        key: 'sky',
-        src: 'src/assets/sky.png',
+        key: 'bear',
+        src: 'src/assets/images/bear.png',
     },
 
+    buffalo: {
+        type: ImageType.Image,
+        key: 'buffalo',
+        src: 'src/assets/images/buffalo.png',
+    },
+
+    chick: {
+        type: ImageType.Image,
+        key: 'chick',
+        src: 'src/assets/images/chick.png',
+    },
+
+    chicken: {
+        type: ImageType.Image,
+        key: 'chicken',
+        src: 'src/assets/images/chicken.png',
+    },
+
+    cow: {
+        type: ImageType.Image,
+        key: 'cow',
+        src: 'src/assets/images/cow.png',
+    },
+
+    crocodile: {
+        type: ImageType.Image,
+        key: 'crocodile',
+        src: 'src/assets/images/crocodile.png',
+    },
+
+    dog: {
+        type: ImageType.Image,
+        key: 'dog',
+        src: 'src/assets/images/dog.png',
+    },
+
+    duck: {
+        type: ImageType.Image,
+        key: 'duck',
+        src: 'src/assets/images/duck.png',
+    },
+
+    elefant: {
+        type: ImageType.Image,
+        key: 'elefant',
+        src: 'src/assets/images/elefant.png',
+    },
+
+    frog: {
+        type: ImageType.Image,
+        key: 'frog',
+        src: 'src/assets/images/frog.png',
+    },
+
+    giraffe: {
+        type: ImageType.Image,
+        key: 'giraffe',
+        src: 'src/assets/images/giraffe.png',
+    },
+
+    goat: {
+        type: ImageType.Image,
+        key: 'goat',
+        src: 'src/assets/images/goat.png',
+    },
+
+    gorilla: {
+        type: ImageType.Image,
+        key: 'gorilla',
+        src: 'src/assets/images/gorilla.png',
+    },
+
+    hippo: {
+        type: ImageType.Image,
+        key: 'hippo',
+        src: 'src/assets/images/hippo.png',
+    },
+
+    horse: {
+        type: ImageType.Image,
+        key: 'horse',
+        src: 'src/assets/images/horse.png',
+    },
+
+    monkey: {
+        type: ImageType.Image,
+        key: 'monkey',
+        src: 'src/assets/images/monkey.png',
+    },
+
+    moose: {
+        type: ImageType.Image,
+        key: 'moose',
+        src: 'src/assets/images/monkey.png',
+    },
+
+    narwhal: {
+        type: ImageType.Image,
+        key: 'narwhal',
+        src: 'src/assets/images/narwhal.png',
+    },
+
+    owl: {
+        type: ImageType.Image,
+        key: 'owl',
+        src: 'src/assets/images/owl.png',
+    },
+
+    panda: {
+        type: ImageType.Image,
+        key: 'panda',
+        src: 'src/assets/images/panda.png',
+    },
+
+    parrot: {
+        type: ImageType.Image,
+        key: 'parrot',
+        src: 'src/assets/images/parrot.png',
+    },
+
+    pengiun: {
+        type: ImageType.Image,
+        key: 'pengiun',
+        src: 'src/assets/images/pengiun.png',
+    },
+
+    pig: {
+        type: ImageType.Image,
+        key: 'pig',
+        src: 'src/assets/images/pig.png',
+    },
+
+    rabbit: {
+        type: ImageType.Image,
+        key: 'rabbit',
+        src: 'src/assets/images/rabbit.png',
+    },
+
+    rhino: {
+        type: ImageType.Image,
+        key: 'rhino',
+        src: 'src/assets/images/rhino.png',
+    },
+
+    sloth: {
+        type: ImageType.Image,
+        key: 'sloth',
+        src: 'src/assets/images/sloth.png',
+    },
+
+    snake: {
+        type: ImageType.Image,
+        key: 'snake',
+        src: 'src/assets/images/snake.png',
+    },
+
+    walrus: {
+        type: ImageType.Image,
+        key: 'walrus',
+        src: 'src/assets/images/walrus.png',
+    },
+
+    whale: {
+        type: ImageType.Image,
+        key: 'whale',
+        src: 'src/assets/images/whale.png',
+    },
+
+    zebra: {
+        type: ImageType.Image,
+        key: 'zebra',
+        src: 'src/assets/images/zebra.png',
+    }
     //#endregion
 
 
